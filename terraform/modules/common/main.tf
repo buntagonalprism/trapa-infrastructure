df727de96d025792cd83de68ea0aefc36d80e269
Alex Bunting <alex.bunting314@gmail.com>
2023-10-03 21:39:09 +1100

    Enable api keys service

--- terraform/modules/common/main.tf
+++ terraform/modules/common/main.tf
@@ -8,6 +8,11 @@ resource "google_project_service" "secrets_service" {
   service = "secretmanager.googleapis.com"
 }
 
+resource "google_project_service" "apikeys_service" {
+  project = var.projectName
+  service = "apikeys.googleapis.com"
+}
+
 resource "google_apikeys_key" "places_api_key" {
   name         = "trapa-places-api-key"
   display_name = "Trapa API Places Service API Key"
