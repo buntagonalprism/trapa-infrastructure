0f1587853bcc9d181e67d58070d306b1ab3de171
Alex Bunting <alex.bunting314@gmail.com>
2023-10-04 14:55:03 +1100

    Add location to prod

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -1,23 +1,24 @@
 terraform {
   backend "gcs" {
-    bucket  = "terraform.trapa-prod.buntagon.com"
-    prefix  = "terraform/state"
+    bucket = "terraform.trapa-prod.buntagon.com"
+    prefix = "terraform/state"
   }
 
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "4.84.0"
     }
   }
 }
 
 provider "google" {
-  project     = "trapa-prod"
-  region      = "australia-southeast1"
+  project = "trapa-prod"
+  region  = "australia-southeast1"
 }
 
 module "common" {
-  source = "../../modules/common"
+  source      = "../../modules/common"
   projectName = "trapa-prod"
+  location    = "australia-southeast1"
 }
